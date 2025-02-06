8166cd50d7c7cab8b06f657884d25523ef07e21c
Micaela Del Longo <micaeladellongo@gmail.com>
2025-02-05 21:20:04 -0300

    move constants file

--- code/constants.py
+++ code/constants.py
@@ -3,11 +3,11 @@ import os
 # PATHS ----------------------------------------------------------------------------------------------------------------
 ROOT = os.path.dirname(os.path.abspath(__file__))
 # dataset
-FLICKR8K_ANN_FILE = "datasets/flickr8k/Flickr8k.token.txt"
-FLICKR8K_CSV_FILE = "datasets/flickr8k/captions.csv"
-FLICKR8K_IMG_DIR = "datasets/flickr8k/images"
+FLICKR8K_ANN_FILE = "../datasets/flickr8k/Flickr8k.token.txt"
+FLICKR8K_CSV_FILE = "../datasets/flickr8k/captions.csv"
+FLICKR8K_IMG_DIR = "../datasets/flickr8k/images"
 # code
-VOCAB_FILE = "code/dataset/vocab.pkl"
+VOCAB_FILE = "dataset/vocab.pkl"
 
 # VOCABULARY -----------------------------------------------------------------------------------------------------------
 # Special tokens
