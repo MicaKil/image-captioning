c7aef86c8dda776f9566954e507112c1e7bb281f
Micaela Del Longo <micaeladellongo@gmail.com>
2025-02-06 21:40:10 -0300

    add checkpoint dir

--- constants.py
+++ constants.py
@@ -8,6 +8,8 @@ FLICKR8K_CSV_FILE = "datasets/flickr8k/captions.csv"
 FLICKR8K_IMG_DIR = "datasets/flickr8k/images"
 # code
 VOCAB_FILE = "code/dataset/vocab.pkl"
+# checkpoints
+CHECKPOINT_DIR = "checkpoints"
 
 # VOCABULARY -----------------------------------------------------------------------------------------------------------
 # Special tokens
@@ -22,5 +24,4 @@ TEST_IMG_CAPTIONS = ["A man plays a song on the guitar for his cat.",
 					 "A man plays a yellow guitar while a cat watches him.",
 					 "A man plays his yellow guitar while staring at his cat.",
 					 "A man, sitting by his computer, playing guitar to his cat.",
-					 "The man is playing guitar and sitting with a cat.",
-					 ]
+					 "The man is playing guitar and sitting with a cat."]
