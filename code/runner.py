454a709675ef4eb7a7b90ffb92b9141aece4c36f
Micaela Del Longo <micaeladellongo@gmail.com>
2025-02-07 19:09:55 -0300

    update training

--- code/runner.py
+++ code/runner.py
@@ -4,11 +4,13 @@ import torch
 from torch.utils.data import random_split
 from torchvision.transforms import v2
 
+from caption import preprocess_image, gen_caption
 from constants import *
 from dataset.flickr_dataloader import FlickerDataLoader
 from dataset.flickr_dataset import FlickerDataset
 from models.basic import ImageCaptioning
 from train import train
+from utils import show_img
 
 # logger
 logger = logging.getLogger(__name__)
@@ -21,7 +23,7 @@ img_dir = str(os.path.join(ROOT, FLICKR8K_IMG_DIR))
 MEAN = [0.485, 0.456, 0.406]
 STD = [0.229, 0.224, 0.225]
 
-transform = v2.Compose([
+TRANSFORM = v2.Compose([
 	v2.ToImage(),
 	v2.Resize((224, 224)),
 	v2.ToDtype(torch.float32, scale=True),
@@ -46,19 +48,23 @@ PIN_MEMORY = True
 # model param
 EMBED_SIZE = 256
 HIDDEN_SIZE = 512
-NUM_LAYERS = 10
-DROPOUT = 0.25
+NUM_LAYERS = 1
+DROPOUT = 0.1
 FREEZE_ENCODER = True
 
 # training params
 ENCODER_LR = 1e-4
-DECODER_LR = 4e-4
+DECODER_LR = 1e-4
 MAX_EPOCHS = 10
 PATIENCE = None
+CALC_BLEU = False
 MAX_CAPTION_LEN = 30
 
+CLIP_GRAD = False
+GRAD_MAX_NORM = 5.0
+
 if __name__ == "__main__":
-	full_dataset = FlickerDataset(ann_file, img_dir, vocab_threshold=VOCAB_THRESHOLD, transform=transform)
+	full_dataset = FlickerDataset(ann_file, img_dir, vocab_threshold=VOCAB_THRESHOLD, transform=TRANSFORM)
 	vocab = full_dataset.vocab
 	pad_idx = vocab.to_idx(PAD)
 
@@ -89,5 +95,12 @@ if __name__ == "__main__":
 		{"params": model.decoder.parameters(), "lr": DECODER_LR},
 	])
 
-	train(model, train_dataloader, val_dataloader, criterion, optimizer, device, vocab, MAX_EPOCHS, PATIENCE,
-		  CHECKPOINT_DIR, MAX_CAPTION_LEN)
+	train(model, train_dataloader, val_dataloader, device, vocab, MAX_EPOCHS, criterion, optimizer, CHECKPOINT_DIR,
+		  CLIP_GRAD, GRAD_MAX_NORM, PATIENCE, CALC_BLEU, MAX_CAPTION_LEN)
+
+	img_path = os.path.join(ROOT, TEST_IMG)
+	img = preprocess_image(str(img_path), TRANSFORM)
+
+	print("Generating image caption.")
+	print(gen_caption(model, img, vocab, max_length=MAX_CAPTION_LEN, device=device, temperature=None))
+	show_img(img, MEAN, STD)
--- code/train.py
+++ code/train.py
@@ -20,28 +20,33 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(format="%(asctime)s | %(levelname)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO)
 
 
-def train(model: nn.Module, train_loader: DataLoader, val_loader: DataLoader, criterion: nn.Module,
-		  optimizer: torch.optim, device: torch.device, vocab: Vocabulary, max_epochs: int, patience: Optional[int],
-		  checkpoint_dir: str, max_caption_len: int = None, calc_bleu=False) -> None:
+def train(model: nn.Module, train_loader: DataLoader, val_loader: DataLoader, device: torch.device, vocab: Vocabulary,
+		  max_epochs: int, criterion: nn.Module, optimizer: torch.optim, checkpoint_dir: str, clip_grad: bool = False,
+		  grad_max_norm: float = None, patience: int = None, calc_bleu: bool = False,
+		  max_caption_len: int = None) -> None:
 	"""
 	Training loop for the model.
 
+	:param grad_max_norm:
 	:param model: The model to train
 	:param train_loader: DataLoader for the training set
 	:param val_loader: DataLoader for the validation set
-	:param criterion: Loss function
-	:param optimizer: Optimizer for training
 	:param device: Device to run the training on
 	:param vocab: Vocabulary object
 	:param max_epochs: Maximum number of epochs to train
+	:param criterion: Loss function
+	:param optimizer: Optimizer for training
+	:param clip_grad:
 	:param patience: Number of epochs to wait for improvement before early stopping
 	:param checkpoint_dir: Directory to save the best model
 	:param max_caption_len: Maximum length of the generated captions
 	:param calc_bleu:
 	:return:
 	"""
+	model_name = model.__class__.__name__
+	model_params = sum(p.numel() for p in model.parameters())
 
-	logger.info(f"Start training for {max_epochs} epochs.")
+	logger.info(f"Start training model {model_name} (Parameters: {model_params}) for {max_epochs} epochs")
 
 	model = model.to(device)
 	best_bleu_score = -np.inf
@@ -50,9 +55,10 @@ def train(model: nn.Module, train_loader: DataLoader, val_loader: DataLoader, cr
 	smoothing = SmoothingFunction().method1
 
 	for epoch in range(max_epochs):
-		train_loss = train_load(criterion, device, epoch, max_epochs, model, optimizer, train_loader)
-		val_loss, blue_score = eval_load(criterion, device, epoch, max_epochs, model, val_loader, vocab,
-										 max_caption_len, smoothing, calc_bleu)
+		train_loss = train_load(model, train_loader, device, epoch, max_epochs, criterion, optimizer, clip_grad,
+								grad_max_norm)
+		val_loss, blue_score = eval_load(criterion, device, epoch, max_epochs, model, val_loader, vocab, calc_bleu,
+										 max_caption_len, smoothing)
 
 		# Calculate epoch metrics
 		train_loss /= len(train_loader.dataset)
@@ -79,23 +85,20 @@ def train(model: nn.Module, train_loader: DataLoader, val_loader: DataLoader, cr
 					break
 
 
-def train_load(criterion: nn.Module,
-			   device: torch.device,
-			   epoch: int,
-			   max_epochs: int,
-			   model: nn.Module,
-			   optimizer: torch.optim,
-			   train_loader: DataLoader):
+def train_load(model: nn.Module, train_loader: DataLoader, device: torch.device, epoch: int, max_epochs: int,
+			   criterion: nn.Module, optimizer: torch.optim, clip_grad: bool, grad_max_norm: Optional[int]) -> float:
 	"""
 	Trains the model on the training set for one epoch
 
-	:param criterion: Loss function
+	:param grad_max_norm:
+	:param model: Model to train
+	:param train_loader: DataLoader for the training set
 	:param device: Device to run the training on
 	:param epoch: Current epoch
 	:param max_epochs: Maximum number of epochs to train
-	:param model: Model to train
+	:param criterion: Loss function
 	:param optimizer: Optimizer for training
-	:param train_loader: DataLoader for the training set
+	:param clip_grad:
 	:return: Total training loss for the epoch
 	"""
 
@@ -107,19 +110,14 @@ def train_load(criterion: nn.Module,
 		captions = captions.to(device)
 
 		# Forward pass
-		loss = forward_pass(captions, criterion, images, model)
+		loss = forward_pass(model, images, captions, criterion)
 
 		# Backward pass
 		optimizer.zero_grad()
 		loss.backward()
-		nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)  # Gradient clipping
+		if clip_grad:
+			nn.utils.clip_grad_norm_(model.parameters(), max_norm=grad_max_norm)  # Gradient clipping
 		optimizer.step()
-		# After optimizer.step():
-		for name, param in model.named_parameters():
-			if param.grad is not None:
-				print(f"{name} gradient norm: {param.grad.norm()}")
-			else:
-				print(f"{name} has no gradients!")
 
 		train_loss += loss.item() * images.size(0)
 		batch_progress.set_postfix({"loss": loss.item()})
@@ -127,8 +125,8 @@ def train_load(criterion: nn.Module,
 
 
 def eval_load(criterion: nn.Module, device: torch.device, epoch: int, max_epochs: int, model: nn.Module,
-			  val_loader: DataLoader, vocab: Vocabulary, max_caption_len: int = None, smoothing: Any = None,
-			  calc_bleu=False) -> tuple:
+			  val_loader: DataLoader, vocab: Vocabulary, calc_bleu: Optional[bool], max_caption_len: Optional[int],
+			  smoothing: Any) -> tuple:
 	"""
 	Evaluates the model on the validation set for one epoch
 
@@ -147,9 +145,10 @@ def eval_load(criterion: nn.Module, device: torch.device, epoch: int, max_epochs
 
 	model.eval()
 	val_loss = 0.0
-	all_references = []  # List of lists of reference captions
-	all_hypotheses = []  # List of generated captions
 	df = val_loader.dataset.df if isinstance(val_loader.dataset, FlickerDataset) else val_loader.dataset.dataset.df
+	if calc_bleu:
+		all_references = []  # List of lists of reference captions
+		all_hypotheses = []  # List of generated captions
 
 	with torch.no_grad():
 		batch_progress = tqdm(val_loader, desc=f"Epoch {epoch + 1}/{max_epochs} [Val]")
@@ -158,9 +157,10 @@ def eval_load(criterion: nn.Module, device: torch.device, epoch: int, max_epochs
 			captions = captions.to(device)
 
 			# Forward pass
-			val_loss = forward_pass(captions, criterion, images, model)
-			val_loss += val_loss.item() * images.size(0)
-			batch_progress.set_postfix({"loss": val_loss.item()})
+			loss = forward_pass(model, images, captions, criterion)
+
+			val_loss += loss.item() * images.size(0)
+			batch_progress.set_postfix({"loss": loss.item()})
 			# Generate captions for BLEU
 			if calc_bleu:
 				captions_for_bleu(all_hypotheses, all_references, device, df, images, images_id, max_caption_len, model,
@@ -172,48 +172,21 @@ def eval_load(criterion: nn.Module, device: torch.device, epoch: int, max_epochs
 	return val_loss, blue_score
 
 
-def forward_pass(captions, criterion, images, model):
+def forward_pass(model, images, captions, criterion):
 	"""
 	Performs a forward pass through the model.
 
-	:param captions: Batch of captions.
-	:param criterion: Loss function.
-	:param images: Batch of images.
+	:param images: Batch of images. Shape: (batch_size, 3, 224, 224)
+	:param captions: Batch of captions. Shape (batch_size, max_caption_length)
 	:param model: The model to perform the forward pass.
+	:param criterion: Loss function.
 	:return: Loss value.
 	"""
-	# Image size: (batch_size, 3, 224, 224) Captions size: (batch_size, max_caption_length)
-	# print(f"\nImages size: {images.size()}")
-	# print(f"Captions size: {captions.size()} || {captions[:, :-1].size()}")
-
-	# Teacher forcing
-	# captions[:, :-1] slices the captions tensor along the time (sequence) dimension to remove the last token from each caption.
-	# Why? In many sequence-to-sequence tasks, you feed the model the caption without its final token so that the model learns to predict the next word at every time step.
-	outputs = model(images, captions[:, :-1])  # Output size: (batch_size, max_caption_length, vocab_size)
-	# print(f"Outputs size: {outputs.size()}")
-	# During evaluation:
-	generated = model(images, captions[:, :-1])
-	print("Model output example:", generated[0].argmax(dim=-1))
-	print("Target example:", captions[0, 1:])
-	print("Output min/max:", outputs.min(), outputs.max())
-	print("Target min/max:", captions[:, 1:].min(), captions[:, 1:].max())
-
-	# This line removes the first time step from the outputs tensor by taking all elements starting from index 1 along the time dimension.
-	# Why?
-	# - The model may output an initial prediction (often corresponding to an artificial or default starting state) that doesn’t correspond to a target token.
-	# - This adjustment aligns the output sequence with the target sequence, which starts at the first true word (excluding the start-of-sequence token).
-	# Now, outputs is modified so that its time dimension matches the target tokens, typically of shape (batch_size, sequence_length - 1, vocab_size).
-	outputs = outputs[:, 1:, :]
-	# print(f"Outputs size: {outputs.size()}")
-
+	outputs = model(images, captions[:, :-1])  # Output shape: (batch_size, max_caption_length, vocab_size)
+	outputs = outputs[:, 1:, :]  # Remove the <SOS> token | Shape: (batch_size, max_caption_length - 1, vocab_size)
 	loss = criterion(
-		# The outputs tensor is reshaped from a 3D tensor (batch_size, sequence_length - 1, vocab_size) to a 2D tensor (batch_size * (sequence_length - 1), vocab_size).
-		# This flattening is required because loss functions like cross-entropy expect a 2D input
-		outputs.reshape(-1, outputs.size(-1)), # (B * seq_len, vocab_size)
-		# captions[:, 1:] slices the original captions tensor to remove the first token, which is usually the Start-Of-Sequence (SOS) token.
-		# This ensures that the target tokens are the ones the model is expected to predict.
-		# captions[:, 1:].reshape(-1): The target tokens are reshaped from (batch_size, sequence_length - 1) to a 1D tensor of size (batch_size * (sequence_length - 1)).
-		captions[:, 1:].reshape(-1)  # Exclude first token (SOS token) # (B * seq_len, vocab_size)
+		outputs.reshape(-1, outputs.size(-1)),
+		captions[:, 1:].reshape(-1)
 	)
 	return loss
 
