8ee2070684beed89bb0b816a0bd10758ade573a7
Micaela Del Longo <micaeladellongo@gmail.com>
2025-02-10 17:19:09 -0300

    remove bleu eval

--- scripts/train.py
+++ scripts/train.py
@@ -1,21 +1,17 @@
 import logging
 import os.path
 import time
-from typing import Optional, Any
+from typing import Optional
 
 import numpy as np
-import pandas as pd
 import torch
 import torch.nn as nn
 import wandb
-from nltk.translate.bleu_score import SmoothingFunction, corpus_bleu
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 from constants import ROOT, PAD
-from scripts.caption import gen_caption
 from scripts.dataset.flickr_dataset import FlickerDataset
-from scripts.dataset.vocabulary import Vocabulary
 from scripts.utils import time_str
 
 logger = logging.getLogger(__name__)
@@ -44,7 +40,6 @@ def train(model: nn.Module, train_loader: DataLoader, val_loader: DataLoader, de
 	)
 	start_time = time.time()
 
-	best_bleu_score = -np.inf
 	best_val_loss = np.inf
 	best_model = None
 	epochs_no_improve = 0
@@ -52,7 +47,7 @@ def train(model: nn.Module, train_loader: DataLoader, val_loader: DataLoader, de
 	model = model.to(device)
 	for epoch in range(config["max_epochs"]):
 		avg_train_loss = train_load(model, train_loader, device, epoch, criterion, optimizer)
-		avg_val_loss, blue_score = eval_load(model, val_loader, device, epoch, criterion, SmoothingFunction().method1)
+		avg_val_loss = eval_load(model, val_loader, device, epoch, criterion)
 
 		logger.info(f"Epoch {epoch + 1} | Train Loss = {avg_train_loss:.4f}, Val Loss = {avg_val_loss:.4f}")
 		wandb.log({
@@ -63,14 +58,6 @@ def train(model: nn.Module, train_loader: DataLoader, val_loader: DataLoader, de
 
 		# Early stopping and checkpointing
 		time_ = time_str()
-		if config["max_caption_len"] is not None:
-			wandb.log({"val_BLEU-4": blue_score})
-			if blue_score > best_bleu_score:
-				best_bleu_score = blue_score
-				if checkpoint_dir is not None:
-					best_model = os.path.join(ROOT, f"{checkpoint_dir}/best_bleu4_{time_}.pt")
-					torch.save(model.state_dict(), best_model)
-				logger.info(f"New best BLEU-4 score: {best_bleu_score:.4f}")
 		if avg_val_loss < best_val_loss:
 			best_val_loss = avg_val_loss
 			epochs_no_improve = 0
@@ -136,12 +123,11 @@ def train_load(model: nn.Module, train_loader: DataLoader, device: torch.device,
 		total_tokens += num_tokens
 		batch_progress.set_postfix({"loss": loss.item() / num_tokens if num_tokens > 0 else 0})
 
-	avg_loss = train_loss / total_tokens if total_tokens > 0 else 0
-	return avg_loss
+	return train_loss / total_tokens if total_tokens > 0 else 0
 
 
-def eval_load(model: nn.Module, val_loader: DataLoader, device: torch.device, epoch: int, criterion: nn.Module,
-			  smoothing: Any) -> tuple:
+def eval_load(model: nn.Module, val_loader: DataLoader, device: torch.device, epoch: int,
+			  criterion: nn.Module) -> tuple:
 	"""
 	Evaluates the model on the validation set for one epoch
 
@@ -150,19 +136,14 @@ def eval_load(model: nn.Module, val_loader: DataLoader, device: torch.device, ep
 	:param device: Device to run the evaluation on
 	:param epoch: Current epoch
 	:param criterion: Loss function
-	:param smoothing: Smoothing function for BLEU score
 	:return: Average validation loss and BLEU score (if calc_bleu is True)
 	"""
 	config = wandb.config
 
 	val_loss = 0.0
 	total_tokens = 0
-	df = val_loader.dataset.df if isinstance(val_loader.dataset, FlickerDataset) else val_loader.dataset.dataset.df
 	vocab = val_loader.dataset.vocab if isinstance(val_loader.dataset,
 												   FlickerDataset) else val_loader.dataset.dataset.vocab
-	pad_idx = vocab.to_idx(PAD)
-	all_references = []  # List of lists of reference captions
-	all_hypothesis = []  # List of generated captions (hypotheses)
 
 	model.eval()
 	with torch.no_grad():
@@ -170,59 +151,12 @@ def eval_load(model: nn.Module, val_loader: DataLoader, device: torch.device, ep
 		for images, captions, images_id in batch_progress:
 			images = images.to(device)
 			captions = captions.to(device)
-
-			loss, num_tokens = forward_pass(model, images, captions, criterion, pad_idx)  # Forward pass
-
+			loss, num_tokens = forward_pass(model, images, captions, criterion, vocab.to_idx(PAD))  # Forward pass
 			val_loss += loss.item()
 			total_tokens += num_tokens
 			batch_progress.set_postfix({"loss": loss.item() / num_tokens if num_tokens > 0 else 0})
 
-			if config["max_caption_len"] is not None:
-				# Generate captions
-				generated = gen_captions(model, vocab, device, images)
-				all_hypothesis.extend(generated)
-				# Process ground truth captions
-				references = get_references(df, images_id)
-				all_references.extend(references)
-
-	avg_loss = val_loss / total_tokens if total_tokens > 0 else 0
-	if config["max_caption_len"] is None:
-		return avg_loss, None
-
-	return avg_loss, corpus_bleu(all_references, all_hypothesis, smoothing_function=smoothing)
-
-
-def get_references(df: pd.DataFrame, image_ids: list) -> list:
-	"""
-	Get references for a list of image ids
-	:param df: DataFrame containing image ids and its captions
-	:param image_ids: List of image ids to get references for
-	:return: List of references for each image id
-	"""
-	references = []
-	for image_id in image_ids:
-		captions = df[df["image_id"] == image_id]["caption"].values
-		references.append([caption.split() for caption in captions])
-	return references
-
-
-def gen_captions(model: nn.Module, vocab: Vocabulary, device: torch.device, images: list) -> list:
-	"""
-	Generate captions for a list of images
-	:param model: Model to use for caption generation
-	:param device: Device to use
-	:param images: List of images to generate captions for
-	:param vocab: Vocabulary of the dataset
-	:return: List of generated captions
-	"""
-	config = wandb.config
-
-	generated = []
-	for image in images:
-		image = image.unsqueeze(0).to(device)
-		caption = gen_caption(model, image, vocab, config["max_caption_len"], device)
-		generated.append(caption.split())
-	return generated
+	return val_loss / total_tokens if total_tokens > 0 else 0
 
 
 def forward_pass(model: nn.Module, images: torch.Tensor, captions: torch.Tensor, criterion: nn.Module,
