f34c922622bdd3b489af761e32fd559394911966
Micaela Del Longo <micaeladellongo@gmail.com>
2025-03-12 13:44:37 -0300

    Format constants

--- constants.py
+++ constants.py
@@ -3,11 +3,15 @@ import os
 # PATHS ----------------------------------------------------------------------------------------------------------------
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
-# dataset
+# datasets
 FLICKR8K_DIR = "data/flickr8k"
 FLICKR8K_ANN_FILE = "data/flickr8k/Flickr8k.token.txt"
 FLICKR8K_CSV_FILE = "data/flickr8k/captions.csv"
 FLICKR8K_IMG_DIR = "data/flickr8k/images"
+
+FLICKR_TRAIN_CSV = "data/flickr8k/train_80_2025-02-16.csv"
+FLICKR_VAL_CSV = "data/flickr8k/val_10_2025-02-16.csv"
+FLICKR_TEST_CSV = "data/flickr8k/test_10_2025-02-16.csv"
 
 COCO_DIR = "data/coco"
 COCO_TRAIN_ANN = "data/coco/annotations/captions_train2014.json"
@@ -21,11 +25,6 @@ COCO_TEST_CSV = "data/coco/coco_test_15.csv"
 COCO_TRAIN_PKL = "data/coco/coco_train.pkl"
 COCO_VAL_PKL = "data/coco/coco_val_15.pkl"
 COCO_TEST_PKL = "data/coco/coco_test_15.pkl"
-
-# dataset splits
-FLICKR_TRAIN_CSV = "data/flickr8k/train_80_2025-02-16.csv"
-FLICKR_VAL_CSV = "data/flickr8k/val_10_2025-02-16.csv"
-FLICKR_TEST_CSV = "data/flickr8k/test_10_2025-02-16.csv"
 
 # checkpoints
 CHECKPOINT_DIR = "checkpoints/"
