99f30f45e51d2665fade1f80c0a189b8c5f71aab
micakil <micaeladellongo@gmail.com>
2025-04-16 00:11:38 -0300

    Add correlation and importance plots

--- plotter/plotter.py
+++ plotter/plotter.py
@@ -1,28 +1,39 @@
 import pandas as pd
 import plotly
 import plotly.express as px
-import plotly.graph_objects as go
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.inspection import permutation_importance
 
 
-def parallel_coordinates(class_name: str = "val_loss.min"):
+def parallel_coordinates(class_name: str = "val_loss.min", model_name: str = "ResNet50-LSTM"):
     df = pd.read_csv("../results/wandb_export_2025-04-13T21_58_18.628-03_00_v3.csv")
 
-    resnet50_lstm_flickr8k = df[(df['encoder'] == 'resnet50') & (df['decoder'] == 'LSTM') & (df['dataset.name'] == 'flickr8k')]
-    resnet50_attention_flickr8k = df[(df['encoder'] == 'resnet50') & (df['decoder'] == 'Attention') & (df['dataset.name'] == 'flickr8k')]
+    columns_to_plot = ['num_layers', 'hidden_size', 'encoder_dropout', 'dropout', 'encoder_lr', 'decoder_lr']
+    match model_name:
+        case "ResNet50-LSTM":
+            model_df = df[(df['encoder'] == 'resnet50') & (df['decoder'] == 'LSTM') & (df['dataset.name'] == 'flickr8k')]
+            columns_to_plot.append('embed_size')
+        case "ResNet50-Attention":
+            model_df = df[(df['encoder'] == 'resnet50') & (df['decoder'] == 'Attention') & (df['dataset.name'] == 'flickr8k')]
+            columns_to_plot.append('num_heads')
+        case _:
+            raise ValueError(f"Invalid model name: {model_name}")
 
-    lstm_columns_to_plot = ['num_layers', 'hidden_size', 'embed_size', 'encoder_dropout', 'dropout', 'encoder_lr', 'decoder_lr']
     match class_name:
         case "val_loss.min":
-            lstm_columns_to_plot.append('val_loss.min')
+            columns_to_plot.append('val_loss.min')
         case "test_CIDEr.max":
-            lstm_columns_to_plot.append('test_CIDEr.max')
+            columns_to_plot.append('test_CIDEr.max')
         case "test_BLEU-4.max":
-            lstm_columns_to_plot.append('test_BLEU-4.max')
+            columns_to_plot.append('test_BLEU-4.max')
         case _:
             raise ValueError(f"Invalid class name: {class_name}")
-    resnet50_lstm_flickr8k = resnet50_lstm_flickr8k[lstm_columns_to_plot]
 
-    lstm_labels = {
+    model_df = model_df[columns_to_plot]
+
+    labels = {
         "num_layers": "Num Layers",
         "hidden_size": "Hidden Size",
         "embed_size": "Embed Size",
@@ -30,25 +41,113 @@ def parallel_coordinates(class_name: str = "val_loss.min"):
         "dropout": "Decoder Dropout",
         "encoder_lr": "Encoder LR",
         "decoder_lr": "Decoder LR",
+        "num_heads": "Num Heads",
         "val_loss.min": "Validation Loss",
         "test_CIDEr.max": "Test CIDEr",
         "test_BLEU-4.max": "Test BLEU-4"
     }
-    fig = px.parallel_coordinates(resnet50_lstm_flickr8k, color=class_name, labels=lstm_labels,
-                                  color_continuous_scale=plotly.colors.sequential.Plotly3)
+    fig = px.parallel_coordinates(model_df, color=class_name, labels=labels, color_continuous_scale=plotly.colors.sequential.Plotly3)
     fig.show()
-    # attention_values = {
-    #     "Num Layers": [1, 2, 3],
-    #     "Hidden Size": [256, 512, 1024],
-    #     "Encoder Dropout": [0.1, 0.3, 0.5],
-    #     "Decoder Dropout": [0.1, 0.3, 0.5],
-    #     "Encoder LR": [1e-5, 5e-5, 0.0001, 0.0005],
-    #     "Decoder LR": [1e-5, 5e-5, 0.0001, 0.0005, 0.001, 0.005],
-    #     "Attention Heads": [1, 2, 4, 8],
-    # }
-    fig2 = go.Figure(data=
-                     go.Parcoords())
+
+
+def correlation_importance(class_name: str = "val_loss.min", model_name: str = "ResNet50-LSTM"):
+    df = pd.read_csv("../results/wandb_export_2025-04-13T21_58_18.628-03_00_v3.csv")
+
+    columns_to_analyze = ['num_layers', 'hidden_size', 'encoder_dropout', 'dropout', 'encoder_lr', 'decoder_lr']
+    match model_name:
+        case "ResNet50-LSTM":
+            model_df = df[(df['encoder'] == 'resnet50') & (df['decoder'] == 'LSTM') & (df['dataset.name'] == 'flickr8k')]
+            columns_to_analyze.append('embed_size')
+        case "ResNet50-Attention":
+            model_df = df[(df['encoder'] == 'resnet50') & (df['decoder'] == 'Attention') & (df['dataset.name'] == 'flickr8k')]
+            columns_to_analyze.append('num_heads')
+        case _:
+            raise ValueError(f"Invalid model name: {model_name}")
+
+    match class_name:
+        case "val_loss.min":
+            columns_to_analyze.append('val_loss.min')
+        case "test_CIDEr.max":
+            columns_to_analyze.append('test_CIDEr.max')
+        case "test_BLEU-4.max":
+            columns_to_analyze.append('test_BLEU-4.max')
+        case _:
+            raise ValueError(f"Invalid class name: {class_name}")
+
+    model_df = model_df[columns_to_analyze]
+
+    labels = {
+        "num_layers": "Num Layers",
+        "hidden_size": "Hidden Size",
+        "embed_size": "Embed Size",
+        "encoder_dropout": "Encoder Dropout",
+        "dropout": "Decoder Dropout",
+        "encoder_lr": "Encoder LR",
+        "decoder_lr": "Decoder LR",
+        "num_heads": "Num Heads",
+        "val_loss.min": "Validation Loss",
+        "test_CIDEr.max": "Test CIDEr",
+        "test_BLEU-4.max": "Test BLEU-4"
+    }
+
+    # Calculate correlations
+    correlation = model_df.corr()[class_name].drop(class_name)
+    correlation.index = correlation.index.map(labels)
+
+    print(f"{labels[class_name]} Correlation and Permutation Importance in {model_name}")
+    print("Correlation with Target:")
+    print(correlation.sort_values(ascending=False).to_string(float_format="%.3f"))
+
+    # Calculate permutation importance
+    features = model_df.drop(columns=[class_name])
+    target = model_df[class_name]
+
+    model = RandomForestRegressor(n_estimators=100, random_state=42)
+    model.fit(features, target)
+
+    result = permutation_importance(model, features, target, n_repeats=10, random_state=42)
+    importance_df = pd.DataFrame({
+        'Feature': features.columns,
+        'Importance': result.importances_mean
+    }).sort_values('Importance', ascending=False)
+    importance_df['Feature'] = importance_df['Feature'].map(labels)
+
+    print("\nPermutation Importance:")
+    print(importance_df.to_string(index=False, float_format="%.3f"))
+
+    diverging_palette = sns.diverging_palette(h_neg=260, h_pos=140, s=80, l=60, n=7, sep=1, center="light")
+
+    plt.figure(figsize=(9, 3))
+    plt.suptitle(f"{labels[class_name]} Correlation and Permutation Importance in {model_name}")
+    plt.subplot(1, 2, 1)
+    ax1 = sns.barplot(x=correlation.values, y=correlation.index, hue=correlation.values, legend=False, palette=diverging_palette)
+    for container in ax1.containers:
+        ax1.bar_label(container, fmt="%.3f", label_type="edge", padding=3, fontsize=9)
+    plt.title(f'Correlation with {labels[class_name]}')
+    plt.xlabel('Pearson Correlation Coefficient')
+    plt.ylabel('Features')
+    plt.axvline(0, color='black', linestyle='--')
+    plt.xlim(-1, 1)
+    plt.grid(axis='x', linestyle='--', alpha=0.5, color='#cccccc')  # X-axis grid
+
+    plt.subplot(1, 2, 2)
+    ax2 = sns.barplot(x='Importance', y='Feature', data=importance_df, hue='Importance', legend=False, palette=diverging_palette)
+    for container in ax2.containers:
+        ax2.bar_label(container, fmt="%.3f", label_type="edge", padding=3, fontsize=9)
+    plt.title('Permutation Importance')
+    plt.xlabel('Importance Score')
+    plt.ylabel('Features')
+    plt.xlim(0, 1.8)
+    plt.grid(axis='x', linestyle='--', alpha=0.5, color='#cccccc')  # X-axis grid
+    plt.tight_layout()
+    # plt.savefig(f"../results/{model_name}_{class_name}_correlation_importance.png", dpi=300, bbox_inches='tight')
+    plt.show()
 
 
 if __name__ == "__main__":
-    parallel_coordinates('test_CIDEr.max')
+    correlation_importance('val_loss.min', 'ResNet50-Attention')
+    correlation_importance('test_CIDEr.max', 'ResNet50-Attention')
+    correlation_importance('test_BLEU-4.max', 'ResNet50-Attention')
+    correlation_importance('val_loss.min', 'ResNet50-LSTM')
+    correlation_importance('test_CIDEr.max', 'ResNet50-LSTM')
+    correlation_importance('test_BLEU-4.max', 'ResNet50-LSTM')
